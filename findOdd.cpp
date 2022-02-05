2833f6028701c4f36d3da323c25d05f5ee07d267
Liron Dubin <ldubin@bu.edu>
2022-02-05 15:34:20 -0500

    This is a working version of desired code.

---
+++ findOdd.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+
+using std::memset;
+using std::cout;
+
+char findOdd(char arr[], int arr_size)
+{    
+    //const char* error = "Error, please enter new characters.";
+    int error = 0;
+    int char_count[26];
+    memset(char_count, 0, sizeof(int) * 26);
+    for (int i = 0; i < arr_size; i++)
+    {
+        char_count[(int) arr[i] - 97] = char_count[(int) arr[i] - 97] + 1;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (char_count[i] % 2 != 0)
+        {
+            int val = i + 97;
+            char ret_char = (char)val;
+            cout << ret_char << " is the ascii value for " << val;
+            return ret_char;
+        }
+    }
+    return error;
+}
+
+int main()
+{
+    char arr[] = {'a', 'a', 'd', 'c', 'd', 'b','f', 'b', 'c', 'd', 'b', 'd', 'b'};
+    int arr_size = sizeof(arr);
+    char ret_char = findOdd(arr, arr_size);
+}
