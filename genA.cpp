b2d569156bda2b4816f29480d90bda96b6cb0afe
Liron Dubin <ldubin@bu.edu>
2022-02-06 14:01:34 -0500

    Code cycles through an appropriate amount of times, but I need to build up - not down.

--- genA.cpp
+++ genA.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
+#include <cstring>
 
+using std::cout;
+using std::memset;
 
-unsigned long genA(int n) {
+unsigned long genA(int n)
+{
 	/* your code here */
 	return 0; /* don't forget to change the return value */
 }
 
-int main(){
-	
+int main()
+{
+	int A1 = 1, A2 = 1;
+	int n = 4;
+	int tracker[n];
+	memset(tracker, 0, sizeof(int) * n);
+	tracker[0] = 1;
+	tracker[1] = 1;
+	int runSUM = 0;
+
+	if (n == 1)
+	{
+		cout << 1;
+		return 1;
+	}
+	else if (n == 2)
+	{
+		cout << 1;
+		return 1;
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			runSUM = runSUM + (tracker[i] * tracker[n - 2 - i]);
+			// tracker
+			cout << "i = " << i << "\n";
+			cout << "tracker[i] = " << tracker[i] << "\n";
+			cout << "tracker[n - 2 - i] = " << tracker[n - 2 - i] << "\n";
+			cout << "runSUM = " << runSUM << "\n";
+
+			if (n - 2 - i == 0)
+			{
+				// cout << runSUM;
+				return runSUM;
+			}
+		}
+	}
 }
