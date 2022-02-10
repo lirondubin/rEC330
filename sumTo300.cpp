86259cd449a415de1b7773a09e3fd091e086f86f
Liron Dubin <ldubin@bu.edu>
2022-02-09 20:15:55 -0500

    Working version with comments.

--- sumTo300.cpp
+++ sumTo300.cpp
@@ -9,7 +9,7 @@ using std::vector;
 
 int sumTo330(vector<int> nums)
 {
-    sort(nums.begin(), nums.end());
+    //sort(nums.begin(), nums.end());
     //for (int i = 0; i < nums.size(); ++i)
     //    cout << tempec[i] << " ";
     //int length = nums.size();
@@ -24,12 +24,22 @@ int sumTo330(vector<int> nums)
 
     for (int first = 0; first < nums.size() - 2; first++)
     {
-        for (int second = 1; second < nums.size() - 1; second++)
+        //cout << "first = " << first << endl;
+        for (int second = first + 1; second < nums.size() - 1; second++)
         {
+            //cout << "first = " << first << endl;
+            //cout << "second = " << second << endl;
             for (int third = second + 1; third < nums.size(); third++)
             {
-                //tempClose = nums[first] + nums[second] + nums[third];
+                cout << "first = " << first << endl;
+                cout << "second = " << second << endl;
+                cout << "third = " << third << endl;
+                tempClose = nums[first] + nums[second] + nums[third];
+                cout << "nums[first] = " << nums[first] << endl;
+                cout << "nums[second] = " << nums[second] << endl;
+                cout << "nums[third] = " << nums[third] << endl;
                 cout << "tempClose = " << tempClose << endl;
+                cout << endl;
                 if (tempClose == 300)
                 {
                     money = 300;
@@ -40,13 +50,13 @@ int sumTo330(vector<int> nums)
                 else if (tempClose > 300 && (abs(tempClose-300) < abs(closeOver-300)))
                 {
                     closeOver = tempClose;
-                    cout << "in closeOver = " << tempClose << endl;
+                    //cout << "in closeOver = " << tempClose << endl;
                 }
                 // else if (tempClose < 300 && (300 - tempClose < 300 - closeUnder))
                 else if (tempClose < 300 && (tempClose > closeUnder))
                 {
                     closeUnder = tempClose;
-                    cout << "in closeUnder = " << closeUnder << endl;
+                    //cout << "in closeUnder = " << closeUnder << endl;
                 }
                 else
                 {
@@ -77,6 +87,6 @@ int sumTo330(vector<int> nums)
 
 int main()
 {
-    vector<int> nums{100, 100, 70, 150};
+    vector<int> nums{100, 90, 70, 150};
     sumTo330(nums);
 }
