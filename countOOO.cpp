4d1de73947fd60fed5f4d8999d59599db07ec6d0
Liron Dubin <ldubin@bu.edu>
2022-03-02 16:47:51 -0500

    Merge sort code.

---
+++ countOOO.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+//#include "problem3a.h"
+
+using namespace std;
+
+// countOOO computes the number of pairs of elements in rankedListA that are out of order in rankedListB
+int countOOO(std::vector<std::string> rankedListA, std::vector<std::string> rankedListB) {
+	return 0; // don't forget to change this
+}
+
+void merge(int arr[], int p, int q, int r) {
+
+    // Create L ← A[p..q] and M ← A[q+1..r]
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    // Maintain current index of sub-arrays and main array
+    int i, j, k, counter;
+    i = 0;
+    j = 0;
+    k = p;
+    counter = 0;
+
+    // Until we reach either end of either L or M, pick larger among
+    // elements L and M and place them in the correct position at A[p..r]
+    while (i < n1 && j < n2) {
+        if (L[i] <= M[j]) {
+            arr[k] = L[i];
+            i++;
+            counter++;
+        } else {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+
+    // When we run out of elements in either L or M,
+    // pick up the remaining elements and put in A[p..r]
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+    std::cout << "counter = " << counter << "\n";
+}
+
+// Divide the array into two subarrays, sort them and merge them
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    // m is the point where the array is divided into two subarrays
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    // Merge the sorted subarrays
+    merge(arr, l, m, r);
+  }
+}
+
+// Print the array
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+}
+
+// Driver program
+int main() {
+    //        {1, 5, 6, 9, 10, 12}
+  //int arr[] = {6, 5, 12, 10, 9, 1};
+  int arr[] = {4,2,3,1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  mergeSort(arr, 0, size - 1);
+
+  cout << "Sorted array: \n";
+  printArray(arr, size);
+  return 0;
+}
