921e935acd66526d9dcec48b450d836cd52ce5cb
Liron Dubin <ldubin@bu.edu>
2022-03-24 00:06:30 -0400

    Turned all relevant code into functions.

--- Homework_4/part_b/testing.cpp
+++ Homework_4/part_b/testing.cpp
@@ -9,9 +9,8 @@
 
 using namespace std;
 
-int main()
+vector<string> convertLink(string s, ifstream &testFile)
 {
-    ifstream testFile("phishing-links-INACTIVE.txt");
     string line;
     vector<string> linksVec;
     int tot_links = 0;
@@ -21,31 +20,33 @@ int main()
     {
         string temp_numString = "";
         int link_length = line.size();
-        for(int i = 0; i < link_length; i++){
+        for (int i = 0; i < link_length; i++)
+        {
             int indNum_num = int(line[i]);
             string indNum_str = to_string(indNum_num);
             temp_numString += indNum_str;
         }
         linksVec.push_back(temp_numString);
     }
-    tot_links = linksVec.size();
-    for (int i = 0; i < tot_links; i++){
+    return linksVec;
+}
+void print_numVec(int tot_links, vector<string> linksVec)
+{
+    int i = 0;
+    for (i = 0; i < tot_links; i++)
+    {
         cout << "[LINK] " << linksVec[i] << endl;
     }
+    cout << endl
+         << "[INFO] Total Links: " << i << endl;
+    return;
 }
 
-// // this block prints intArr
-// i = 0;
-// j = 0;
-// testFile.clear();
-// testFile.seekg(0, ios::beg);
-// while (getline(testFile, line))
-// {
-//     link_length = line.size();
-//     for (int j = 0; j < link_length; j++)
-//     {
-//         cout << intArr[i][j];
-//     }
-//     cout << endl;
-//     i++;
-// }
+int main()
+{
+    ifstream testFile("phishing-links-INACTIVE.txt");
+    vector<string> linksVec = convertLink("phishing-links-INACTIVE.txt", testFile);
+    int tot_links = linksVec.size();
+    print_numVec(tot_links, linksVec);
+    return 0;
+}
