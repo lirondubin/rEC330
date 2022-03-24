8bb02f48a0379612949f80066813cfedbe67d8e8
Liron Dubin <ldubin@bu.edu>
2022-03-23 23:41:51 -0400

    New version of converting to ascii. This version stores each link as a string of numbers in a vector of strings.

--- Homework_4/part_b/testing.cpp
+++ Homework_4/part_b/testing.cpp
@@ -11,49 +11,27 @@ using namespace std;
 
 int main()
 {
-
-    string yeet = "phishing-links-INACTIVE.txt";
-    int string_length = yeet.size();
-    string temp_numString = "";
-    for(int i = 0; i < string_length; i++){
-        int indNum_num = int(yeet[i]);
-        string indNum_str = to_string(indNum_num);
-        temp_numString += indNum_str;
+    ifstream testFile("phishing-links-INACTIVE.txt");
+    string line;
+    vector<string> linksVec;
+    int tot_links = 0;
+    int link_length = 0;
+    int max_length = 0;
+    while (getline(testFile, line))
+    {
+        string temp_numString = "";
+        int link_length = line.size();
+        for(int i = 0; i < link_length; i++){
+            int indNum_num = int(line[i]);
+            string indNum_str = to_string(indNum_num);
+            temp_numString += indNum_str;
+        }
+        linksVec.push_back(temp_numString);
     }
-    cout << temp_numString << endl;
-
-    // ifstream testFile("phishing-links-INACTIVE.txt");
-    // string line;
-    // vector<int> linkVec;
-    // int tot_links = 0;
-    // int link_length = 0;
-    // int max_length = 0;
-    // int i = 0;
-    // int j = 0;
-    // while (getline(testFile, line))
-    // {
-    //     tot_links++;
-    //     link_length = line.size();
-    //     if (link_length > max_length)
-    //     {
-    //         max_length = link_length;
-    //     }
-    // }
-    // int intArr[tot_links][max_length]; // sizeof(arr)/sizeof(arr[0])
-
-    // i = 0;
-    // testFile.clear();
-    // testFile.seekg(0, ios::beg);
-    // while (getline(testFile, line))
-    // {
-    //     link_length = line.size();
-    //     vector<int> intVec(0, link_length);
-    //     for (int j = 0; j < link_length; j++)
-    //     {
-    //         intArr[i][j] = int(line[j]);
-    //     }
-    //     i++;
-    // }
+    tot_links = linksVec.size();
+    for (int i = 0; i < tot_links; i++){
+        cout << "[LINK] " << linksVec[i] << endl;
+    }
 }
 
 // // this block prints intArr
