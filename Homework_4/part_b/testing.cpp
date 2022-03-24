4db3b85d6cc2d2c6f8eaa35ced641dc1e55ff4f9
Liron Dubin <ldubin@bu.edu>
2022-03-23 23:29:06 -0400

    Before deleting old 2D array of vectors of ints.

--- Homework_4/part_b/testing.cpp
+++ Homework_4/part_b/testing.cpp
@@ -5,45 +5,55 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
-    ifstream testFile("phishing-links-INACTIVE.txt");
-    string line;
-    vector<int> linkVec;
-    int tot_links = 0;
-    int link_length = 0;
-    int max_length = 0;
-    int i = 0;
-    int j = 0;
-    while (getline(testFile, line))
-    {
-        tot_links++;
-        link_length = line.size();
-        if (link_length > max_length)
-        {
-            max_length = link_length;
-        }
-    }
-    int intArr[tot_links][max_length]; // sizeof(arr)/sizeof(arr[0])
 
-    // for (int i = 0; i < tot_links; i++)
-    i = 0;
-    testFile.clear();
-    testFile.seekg(0, ios::beg);
-    while (getline(testFile, line))
-    {
-        link_length = line.size();
-        vector<int> intVec(0, link_length);
-        for (int j = 0; j < link_length; j++)
-        {
-            intArr[i][j] = int(line[j]);
-        }
-        i++;
+    string yeet = "phishing-links-INACTIVE.txt";
+    int string_length = yeet.size();
+    string temp_numString = "";
+    for(int i = 0; i < string_length; i++){
+        int indNum_num = int(yeet[i]);
+        string indNum_str = to_string(indNum_num);
+        temp_numString += indNum_str;
     }
+    cout << temp_numString << endl;
+
+    // ifstream testFile("phishing-links-INACTIVE.txt");
+    // string line;
+    // vector<int> linkVec;
+    // int tot_links = 0;
+    // int link_length = 0;
+    // int max_length = 0;
+    // int i = 0;
+    // int j = 0;
+    // while (getline(testFile, line))
+    // {
+    //     tot_links++;
+    //     link_length = line.size();
+    //     if (link_length > max_length)
+    //     {
+    //         max_length = link_length;
+    //     }
+    // }
+    // int intArr[tot_links][max_length]; // sizeof(arr)/sizeof(arr[0])
+
+    // i = 0;
+    // testFile.clear();
+    // testFile.seekg(0, ios::beg);
+    // while (getline(testFile, line))
+    // {
+    //     link_length = line.size();
+    //     vector<int> intVec(0, link_length);
+    //     for (int j = 0; j < link_length; j++)
+    //     {
+    //         intArr[i][j] = int(line[j]);
+    //     }
+    //     i++;
+    // }
 }
 
 // // this block prints intArr
