b15deb0d0a98ace1feeb0535dbd1c46b3a056462
Liron Dubin <ldubin@bu.edu>
2022-03-24 00:32:34 -0400

    Night time.

--- Homework_4/part_b/testing.cpp
+++ Homework_4/part_b/testing.cpp
@@ -6,13 +6,14 @@
 #include <iostream>
 #include <sstream>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
 vector<string> convertLink(string s, ifstream &testFile)
 {
     string line;
-    vector<string> linksVec;
+    vector<string> linksVec_string;
     int tot_links = 0;
     int link_length = 0;
     int max_length = 0;
@@ -26,11 +27,19 @@ vector<string> convertLink(string s, ifstream &testFile)
             string indNum_str = to_string(indNum_num);
             temp_numString += indNum_str;
         }
-        linksVec.push_back(temp_numString);
+        linksVec_string.push_back(temp_numString);
     }
-    return linksVec;
+    return linksVec_string;
 }
-void print_numVec(int tot_links, vector<string> linksVec)
+vector<int> string_to_int(vector<string> linksVec_string){
+    vector<int> linksVec_int;
+    for(int i = 0; i < linksVec_string.size(); i++){
+        int theNum = stoi(linksVec_string[i]);
+        linksVec_int.push_back(theNum);
+    }
+    return linksVec_int;
+}
+void print_numVec(int tot_links, vector<int> linksVec)
 {
     int i = 0;
     for (i = 0; i < tot_links; i++)
@@ -45,8 +54,12 @@ void print_numVec(int tot_links, vector<string> linksVec)
 int main()
 {
     ifstream testFile("phishing-links-INACTIVE.txt");
-    vector<string> linksVec = convertLink("phishing-links-INACTIVE.txt", testFile);
-    int tot_links = linksVec.size();
-    print_numVec(tot_links, linksVec);
+    vector<string> linksVec_string = convertLink("phishing-links-INACTIVE.txt", testFile);
+    int tot_links = linksVec_string.size();
+    vector<int> linksVec_int = string_to_int(linksVec_string);
+    print_numVec(tot_links, linksVec_int);
+    // int theNum = stoi(linksVec[1]);
+    // sscanf(linksVec[1], "%d", &theNum);
+    // stringsteam theNum(linksVec[1]);
     return 0;
 }
