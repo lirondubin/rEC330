3319f8abbdd2e88f514fef099498324244a24b0c
Liron Dubin <ldubin@bu.edu>
2022-04-21 18:57:04 -0400

    I have no idea how to fix this. Gonna give up.

--- Homework_6/Question 4/job.cpp
+++ Homework_6/Question 4/job.cpp
@@ -16,7 +16,6 @@ set<int> white; // white container
 set<int> grey;  // grey container
 set<int> black; // black container
 int flag = 0;
-int flag1 = 0;
 
 void CycleDetect(int n, vector<int> adj[]);
 bool canFinish(int n, vector<pair<int, int>> &dependencies);
@@ -51,6 +50,7 @@ bool canFinish(int n, vector<pair<int, int>> &dependencies)
     // cout << "[INFO] dependencies.size() = " << dependencies.size() << endl;
     for (int i = 0; i < dependencies.size(); i++)
     {
+        //{1,2}
         adj[dependencies[i].first].push_back(dependencies[i].second);
     }
     for (int i = 1; i < n; i++)
@@ -81,75 +81,112 @@ bool canRun(int n, vector<pair<int, int>> &dependencies, int j, int i)
     // reverse dependicnes
     vector<int> opppsiteAdj[n + 1]; // array of vectors to store the graph
     stack<int> stack;
+    vector<int> visited(n + 1, 0);
 
     for (int i = 0; i < dependencies.size(); i++)
     {
         opppsiteAdj[dependencies[i].second].push_back(dependencies[i].first);
     }
-    white.erase(n); // remove from the white set
-    grey.insert(n); // put into the grey set
+    // white.erase(n); // remove from the white set
+    // grey.insert(n); // put into the grey set
     stack.push(n);
 
-    int counter = 0;
+    //{2,1}
+    //{1,2}
+    // {{1,2},{2,3},{2,4},{1,5}};
+    // {{2,1},{3,2},{4,2},{5,1}};
+    int counter = 1;
     while (!stack.empty())
     {
-        int popped = stack.top();
-        stack.pop();
-        if (popped == *(white.end()))
+        // int popped = stack.top();
+        // stack.pop();
+        // cout << "[INFO] popped = " << popped << endl;
+        // // {1,2}
+        // // {2,1}
+        // // {2,3}
+        // if (!visited[popped])
+        // {
+        //     visited[popped] = 1;
+        // }
+        for (int k = 1; k < opppsiteAdj[popped].size() + 1; k++)
         {
-            for (int i = 0; i < opppsiteAdj[n].size(); i++)
+            cout << "[INFO] opppsiteAdj[popped][k] " << opppsiteAdj[popped][k] << endl;
+            // counter++;
+            cout << "[INFO] visited[k] = " << visited[k] << endl;
+            if (visited[k])
             {
-                grey.insert(n);
-                stack.push(n);
-                if (counter > i)
-                { // check if it is present or not in grey set, if present then return true
-                    // flag1 = 1;
-                    // cout << "TRUE - This can run" << endl;
-                    return false;
-                }
+                counter++;
+            }
+            else
+            {
+                stack.push(k);
             }
         }
-        black.insert(n); // put into the black set
-        grey.erase(n);
-        counter++;
     }
-    return true;
+    cout << endl;
+    if (i >= counter)
+    { // check if it is present or not in grey set, if present then return true
+        // flag1 = 1;
+        cout << "TRUE - This CAN run" << endl;
+        return true;
+    }
+    else
+    {
+        cout << "FALSE - This CAN NOT run" << endl;
+        return false;
+    }
 }
 
-// int main()
-// {
-//     vector<pair<int, int>> CircleLine = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {13, 14}, {14, 15}, {15, 16}, {16, 17}, {17, 18}, {18, 19}, {19, 20}, {20, 21}, {21, 22}, {9, 1}};
-//     canFinish(22, CircleLine); // expect false
+int main()
+{
+    // vector<pair<int, int>> CircleLine = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {13, 14}, {14, 15}, {15, 16}, {16, 17}, {17, 18}, {18, 19}, {19, 20}, {20, 21}, {21, 22}, {9, 1}};
+    // canFinish(22, CircleLine); // expect false
 
-//     // vector<pair<int, int>> CircleLine = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{8,9},{9,1},{9,10}/*,{14,15},{15,16},{16,17},{17,18},{18,19},{19,20},{20,21},{21,22}*/};
-//     // canFinish(22, CircleLine); // expect false
+    // vector<pair<int, int>> CircleLine = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{8,9},{9,1},{9,10}/*,{14,15},{15,16},{16,17},{17,18},{18,19},{19,20},{20,21},{21,22}*/};
+    // canFinish(22, CircleLine); // expect false
 
-//     // vector<pair<int, int>> circle = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{8,9},{9,1}};
-//     // canFinish(9, circle); // expect false
+    // vector<pair<int, int>> circle = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{8,9},{9,1}};
+    // canFinish(9, circle); // expect false
 
-//     // vector<pair<int, int>> line = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{8,9},{9,10}};
-//     // canFinish(10, line); // expect true
+    // vector<pair<int, int>> line = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{8,9},{9,10}};
+    // canFinish(10, line); // expect true
 
-//     // vector<pair<int, int>> star1 = {{10,2},{10,2},{10,3},{10,4},{10,5},{10,6},{10,7},{10,8},{10,9}};
-//     // canFinish(10, star1); // expect true
+    // vector<pair<int, int>> star1 = {{10,2},{10,2},{10,3},{10,4},{10,5},{10,6},{10,7},{10,8},{10,9}};
+    // canFinish(10, star1); // expect true
 
-//     // vector<pair<int, int>> star2 = {{1,10},{2,10},{3,10},{4,10},{5,10},{6,10},{7,10},{8,10},{9,10}};
-//     // canFinish(10, star2); // expect true
+    // vector<pair<int, int>> star2 = {{1,10},{2,10},{3,10},{4,10},{5,10},{6,10},{7,10},{8,10},{9,10}};
+    // canFinish(10, star2); // expect true
 
-//     // vector<pair<int, int>> ShortLoop = {{1,2},{2,3},{3,1}};
-//     // canFinish(3, ShortLoop); // expect false
+    // vector<pair<int, int>> ShortLoop = {{1,2},{2,3},{3,1}};
+    // canFinish(3, ShortLoop); // expect false
 
-//     // vector<pair<int, int>> small = {{1,2},{2,3}};
-//     // canFinish(2, small); // expect true
+    // vector<pair<int, int>> small = {{1,2},{2,3}};
+    // canFinish(2, small); // expect true
 
-//     // vector<pair<int, int>> ex2 = {{1, 2}, {2, 1}};
-//     // canFinish(2, ex2); // expect true
+    // vector<pair<int, int>> ex2 = {{1, 2}, {2, 1}};
+    // canFinish(2, ex2); // expect true
 
-//     // vector<pair<int, int>> ex1 = {{1,2}};
-//     // canFinish(2,ex1);
+    // vector<pair<int, int>> ex1 = {{1, 2}};
+    // canFinish(2, ex1);
+    // cout << canRun(2, ex1, 2, 1) << endl;
 
-//     // vector<pair<int, int>> test = {{1, 2}, {2, 3}, {3, 1}, {3, 4}};
-//     // canFinish(3, test);
+    // vector<pair<int, int>> test = {{1, 2}, {2, 3}, {3, 1}, {3, 4}};
+    // canFinish(3, test);
 
-//     return 0;
-// }
+    // vector<pair<int, int>> telephone9 = {{1, 2}, {2, 3}, {4, 5}, {5, 6}, {7, 8}, {8, 9}, {1, 4}, {2, 5}, {3, 6}, {4, 7}, {5, 8}, {6, 9}};
+    // cout << canRun(9, telephone9, 9, 4) << endl;
+
+    // vector<pair<int, int>> telephone8 = {{1, 2}, {2, 3}, {4, 5}, {5, 6}, {7, 8}, {8, 9}, {1, 4}, {2, 5}, {3, 6}, {4, 7}, {5, 8}, {6, 9}};
+    // // cout << canRun(9, telephone8, 8, 6) << endl;
+    // canRun(9, telephone8, 8, 6);
+
+    // vector<pair<int, int>> exFalse = {{1, 2}};
+    // // cout << canRun(2, exFalse, 2, 1) << endl;
+    // canRun(2, exFalse, 2, 1);
+
+    vector<pair<int, int>> exFalse = {{1, 2}, {2, 3}, {2, 4}, {1, 5}};
+    // cout << canRun(2, exFalse, 2, 1) << endl;
+    canRun(5, exFalse, 3, 2);
+
+    return 0;
+}
